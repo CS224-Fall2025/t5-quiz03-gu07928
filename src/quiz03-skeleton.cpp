2a5bf273e46ac9fe11163d09c40d171797f98f53
Ghos Ahmed Usmani <gu07928@st.habib.edu.pk>
2025-11-14 09:26:33 +0500

    quiz

--- src/quiz03-skeleton.cpp
+++ src/quiz03-skeleton.cpp
@@ -1,5 +1,47 @@
 #include <iostream>
 
-int main() {
+class employee {
+    protected :
+        string name;
+        double salary;
+        static int empCount = 0;
+    
+    public:
+        employee(string n, double s) {name = n; salary = s; empCount++}
+        virtual void distplayInfo() {std::cout << name << salary;}
+        static void showTotalEmp() {std::cout << empCount << endl;}
+        virtual ~employee();
+};
 
+class staff: public employee {
+    private:
+        string department = "Staff";
+    public:
+        void distplayInfo() override {
+            std::cout << department << name << salary;
+        }
+};
+
+class faculty: public employee {
+    private:
+        string department = "Faculty";
+    public:
+        void distplayInfo() override {
+            std::cout << department << name << salary;
+        }
+};
+
+int main() {
+    int n;
+    cin >> n;
+    string t, m;
+    int sal;
+    for (int i = 0; i < n; i++) {
+        cin >> t >> n >> sal;
+        if (t == "Staff") {
+
+        } else {
+
+        }
+    }
 }
